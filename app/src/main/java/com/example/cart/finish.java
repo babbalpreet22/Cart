d6e1763e78c994e970df4831d30c8a0bae54ad63
amanpreetkaur005 <amanpreetkaur00500@gmail.com>
2020-02-16 23:40:39 -0500

    Initial commit

---
+++ app/src/main/java/com/example/cart/finish.java
@@ -0,0 +1,16 @@
+package com.example.cart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class finish extends AppCompatActivity {
+TextView message;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_finish);
+        message=findViewById(R.id.message);
+    }
+}
---
+++ app/src/main/java/com/example/cart/retrieve.java
@@ -0,0 +1,59 @@
+package com.example.cart;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+public class retrieve extends AppCompatActivity {
+    TextView text1; Button retrieve_button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_retrieve);
+        text1=findViewById(R.id.text1);
+        retrieve_button=findViewById(R.id.retrieve_button);
+
+        retrieve_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getData();
+            }
+        });
+    }
+
+    private void getData() {
+        FirebaseFirestore db=FirebaseFirestore.getInstance();
+        db.collection("Orders")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if(task.isSuccessful()){
+                            for(QueryDocumentSnapshot document:task.getResult()){
+                                System.out.println(document.getString("Price"));
+                            }
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(retrieve.this,e.getMessage(),Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+    }
+}
